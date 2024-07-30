f050bd4a630e8afd6482b2725e32c6e99a82229a
Eze Richard <richardeze391@gmail.com>
2024-07-30 07:11:41 +0100

    updating my employee page

--- dist/script/script.js
+++ dist/script/script.js
@@ -6,39 +6,4 @@ toggleSideBar.addEventListener('click', () => {
     sideBar.classList.toggle('toggle_side');
 });
 
-document.querySelectorAll(".menu_item").forEach(item => {
-    item.addEventListener('mouseover', () => {
-        document.querySelectorAll(".menu_item").forEach(el=>el.classList.remove("active_nav"))
-        item.classList.add("active_nav");
-    });
-
-    item.addEventListener('mouseleave', () => {
-        item.classList.remove('active');
-      });
-});
-
-let currentActive = document.querySelector('.menu_item.active_nav')
-
-document.querySelectorAll('.menu_item').forEach(item => {
-    item.addEventListener('click', () => {
-      if (currentActive) {
-        currentActive.classList.remove('active');
-      }
-      item.classList.add('active');
-      currentActive = item;
-    });
-
-    item.addEventListener('mouseover', () => {
-      if (currentActive !== item) {
-        item.classList.add('active');
-      }
-    });
-
-    item.addEventListener('mouseleave', () => {
-      if (currentActive !== item) {
-        item.classList.remove('active');
-      }
-    });
-  });
-
-
+ 
