cf8faf3c4fb2890b398c01dbb80fee29bdafc4f4
Eze Richard <richardeze391@gmail.com>
2024-07-30 14:14:39 +0100

    added profile and payroll file

--- dist/script/script.js
+++ dist/script/script.js
@@ -6,4 +6,34 @@ toggleSideBar.addEventListener('click', () => {
     sideBar.classList.toggle('toggle_side');
 });
 
- 
+//  select_all
+// select_employee
+
+document.querySelector('.select_all').addEventListener('change', function() {
+    // Get all checkboxes with class 'row-checkbox'
+    const checkboxes = document.querySelectorAll('.select_employee');
+    
+    // Set the checked state to match the header checkbox
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = this.checked;
+    });
+});
+
+
+
+
+const currentMonthIndex = new Date().getMonth();
+        
+// Array of month values matching the options
+const monthValues = [
+    "january", "february", "march", "april", "may", "june",
+    "july", "august", "september", "october", "november", "december"
+];
+
+// Get the select element
+const selectElement = document.getElementById("monthly_filter");
+
+// Set the selected index based on the current month
+selectElement.value = monthValues[currentMonthIndex];
+
+
